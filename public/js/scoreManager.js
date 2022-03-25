3ea325b5a91ded95c125a8245daec44c6bed5e69
Kedar Kshatriya <kshatriyakedar@gmail.com>
2022-03-26 01:24:08 +0530

    added perks board

--- public/js/scoreManager.js
+++ public/js/scoreManager.js
@@ -12,11 +12,13 @@ async function getScores(ethAddress) {
       stepTime = 200;
       killCounter = 0;
     } else if (response?.data?.rows) {
-      console.log("response.randVRFNumber",response.randVRFNumber);
-      stepTime = 200 - (response.randVRFNumber * 10);
+      console.log("response.randVRFNumber", response.randVRFNumber);
+      stepTime = 200 - ((response.randVRFNumber == 0 ? 1 : response.randVRFNumber) * 10);
       console.log("stepTime", stepTime);
-      killCounter = response.randVRFNumber * 100;
+      killCounter = (response.randVRFNumber == 0 ? 1 : response.randVRFNumber) * 100;
       console.log("killCounter", killCounter);
+      $('#perksString').text(`Perks Enabled:`);
+      $('#perksString2').text(`Speed Increase of ${200 % stepTime}%`);
     }
     return true;
   });
@@ -50,6 +52,8 @@ async function delScores(ethAddress) {
     if (response) {
       killCounter = 0;
       stepTime = 200;
+      $('#perksString').text(`Perks Disabled`);
+      $('#perksString2').text(``);
     }
   });
 }
